b68e791122c925a5559d55c281f849555988f841
Advait Parashar <advaitparashar2@gmail.com>
2025-01-17 10:15:57 +0530

    Add files via upload
    A simple List problem of asking user for inputs of his 3 fav. movies and then displaying them as a list.

---
+++ List problem1.py
@@ -0,0 +1,6 @@
+#WAP a user to input his 3 fav. movies and store them together in a list.
+a= str(input("1st fav. movie: "))
+b= str(input("2nd fav. movie: "))
+c= str(input("3rd fav. movie: "))
+List=[a,b,c]
+print(List)

1be770af2f5df8febbb3d88ae4258b08716bfb93
Advait Parashar <advaitparashar2@gmail.com>
2025-01-17 10:31:48 +0530

    Add files via upload
    #An alternate solution to the same problem

---
+++ List problem 1 alt.py
@@ -0,0 +1,10 @@
+#An alternate solution to the same problem
+fav_movies=[]
+mov=input("1st fav. movie: ")
+fav_movies.append(mov)
+mov=input("2nd fav. movie: ")
+fav_movies.append(mov)
+mov=input("3rd fav. movie: ")
+fav_movies.append(mov)
+
+print(fav_movies)
